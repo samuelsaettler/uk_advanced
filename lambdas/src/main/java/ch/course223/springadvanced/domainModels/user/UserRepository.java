ef990cf42d4842d6bb43d76740e53677b262b864
Samuel Sättler <samuel.saettler@edu.tbz.ch>
2020-04-15 08:37:46 +0200

    lambdas in seperate branch

--- lambdas/src/main/java/ch/course223/springadvanced/domainModels/user/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package ch.course223.springadvanced.domainModels.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User,String> {
-}
