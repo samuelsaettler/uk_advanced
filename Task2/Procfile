1ed2d14fa5fb7c35bdffef1c3d75b5ba30f95cec
Samuel Sättler <samuel.saettler@edu.tbz.ch>
2020-04-15 08:35:49 +0200

    Cleanup the branch

--- Task2/Procfile
+++
@@ -1,1 +0,0 @@
-web: ./gradlew bootJar && java -Dserver.port=$PORT $JAVA_OPTS -jar build/libs/app.jar --spring.datasource.url=${JDBC_DATABASE_URL}
--- Task2/src/main/java/ch/course223/advanced/domainmodels/user/UserService.java
+++
@@ -1,8 +0,0 @@
-package ch.course223.advanced.domainmodels.user;
-
-import ch.course223.advanced.core.ExtendedService;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService extends UserDetailsService, ExtendedService<User> {
-    User findByEmail(String email);
-}
